03d68f770580e6a43ce34c2d417853ae0e9e0f33
Marvin Schopf <marvin@schopf.biz>
2021-02-24 21:41:19 +0100

    Prettify files

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const resolveEnvVars = require('./resolve-env-vars');
-const { DefinePlugin } = require('webpack');
+const resolveEnvVars = require("./resolve-env-vars");
+const { DefinePlugin } = require("webpack");
 
 module.exports = (config, env, helpers) => {
-  const { stringified, raw } = resolveEnvVars('PREACT_APP_');
-  config.plugins.push(new DefinePlugin(stringified));
+	const { stringified, raw } = resolveEnvVars("PREACT_APP_");
+	config.plugins.push(new DefinePlugin(stringified));
 
-  const { plugin: htmlPlugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0] || {};
+	const { plugin: htmlPlugin } =
+		helpers.getPluginsByName(config, "HtmlWebpackPlugin")[0] || {};
 
-  if (htmlPlugin) {
-    // Pass all prefixed env vars to the HTML template.
-    htmlPlugin.options.env = raw;
-  }
+	if (htmlPlugin) {
+		// Pass all prefixed env vars to the HTML template.
+		htmlPlugin.options.env = raw;
+	}
 };
